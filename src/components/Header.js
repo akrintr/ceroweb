945191b5cebe7ad6805194e4aa9056fb022eb69d
akrintr <41724889+akrintr@users.noreply.github.com>
2018-07-27 07:02:10 +0700

    state lesson

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
-import React from "react";
-const Header = () =>{
-    return <h1>Header</h1>
+import React,{Component} from "react";
+
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state = {date : new Date()};
+        setInterval(() => this.tick(),1000);
+    }
+
+    tick(){
+        this.setState({date : new Date()});
+    }
+
+    render(){
+        return (
+            <div>{this.state.date.toLocaleTimeString()}</div>
+        )
+    }
+
 }
 
 export default Header;
