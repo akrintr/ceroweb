9946d32743b5e8af0d9d63800ead11b50b763141
akrintr <41724889+akrintr@users.noreply.github.com>
2018-07-27 07:16:09 +0700

    state life cycle

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,24 @@ class Header extends Component{
     constructor(props){
         super(props);
         this.state = {date : new Date()};
-        setInterval(() => this.tick(),1000);
+        
+
+        console.log('constructor');
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(() => this.tick(),1000);
+        console.log('mount');
+
+    }
+
+    componentDidUpdate(){
+        console.log('update');
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+        console.log('unmount');
     }
 
     tick(){
